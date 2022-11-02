8b8d7ba0dca60e572b79fe1106bf57503f4866e4
gyeongdeang <joohyeon0286@gmail.com>
2022-11-02 23:46:41 +0900

    11월 2일 알고리즘 문제 풀이

--- src/firstweek/BJ14681.java
+++ src/firstweek/BJ14681.java
@@ -3,8 +3,22 @@ package firstweek;
 import java.util.Scanner;
 
 public class BJ14681 {
-    Scanner scanner = new Scanner(System.in);
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
+    public static void main(String[] args) {
 
+
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (0 < x && 0 < y) {
+            System.out.println("제1사분면");
+        } else if (x < 0 && 0 < y) {
+            System.out.println("제2사분면");
+        } else if (x < 0 && y < 0) {
+            System.out.println("제3사분면");
+        } else if (0 < x && y < 0) {
+            System.out.println("제4사분면");
+        }
+
+    }
 }
